a8e1379815cb7c4884c3c0d04a5e81df3529062a
yank05 <koenigyannik@web.de>
2022-06-20 20:10:05 +0200

    ADD

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Function für laden der Screens bei jeweiliger Auswahl der Stufe
+//Funktion für Laden der Screens bei jeweiliger Auswahl der Stufe
 document.getElementById("b1").addEventListener("click", LoadLevels);
 //Alle Elemente der Startseite als Objekt gespeichert
 const MainPageInterface = {
@@ -11,12 +11,29 @@ const MainPageInterface = {
     button3: document.getElementById("b3"),
 };
 //Deklaration der Sätze
-const sentence1 = {
+const sentences = [];
+sentences[0] = {
     translation: "Mir geht es gut",
-    word1: "Yo",
+    word1: "yo",
     word2: "soy",
     word3: "así",
-    wordCount: 3,
+    WordNumber: 3,
+};
+sentences[1] = {
+    translation: "Es ist sehr warm heute",
+    word1: "hace",
+    word2: "mucho",
+    word3: "calor",
+    word4: "hoy",
+    WordNumber: 4,
+};
+sentences[2] = {
+    translation: "Die Rechnung bitte",
+    word1: "La",
+    word2: "cuenta,",
+    word3: "por",
+    word4: "favor",
+    WordNumber: 4,
 };
 //wechselt Interface
 function LoadLevels() {
@@ -27,20 +44,44 @@ function LoadLevels() {
     MainPageInterface.button1.remove();
     MainPageInterface.button2.remove();
     MainPageInterface.button3.remove();
-    //Erstellung der Überschrift
-    let Text1 = document.createElement("h2");
-    Text1.setAttribute("id", "translationHeader");
-    Text1.innerHTML = "Auf deutsch:";
-    document.getElementById("content").appendChild(Text1);
     //Erstellung der Aufforderung
     let Text2 = document.createElement("h3");
     Text2.setAttribute("id", "taskText"),
         Text2.innerHTML = "Wähle die übersetzten Wörter in der richtigen Reihenfolge";
     document.getElementById("content").appendChild(Text2);
+    //Zufallsgenerator welcher Satz kommt
+    let minSentence = 0;
+    let maxSentence = 2;
+    let randomSentenceSelector = Math.floor(Math.random() * (maxSentence - minSentence + 1)) + minSentence;
+    console.log(randomSentenceSelector);
     //Erstellung des Feldes mit der deutschen Übersetzung 
     let translationGerman = document.createElement("div");
     translationGerman.setAttribute("id", "translationGerman");
-    translationGerman.innerHTML = sentence1.translation;
+    translationGerman.innerHTML = sentences[randomSentenceSelector].translation;
     document.getElementById("content").appendChild(translationGerman);
+    //Zufallsgenerator wo welches Wort steht 
+    // Wort 1
+    let minWord = 1;
+    let maxWord1 = sentences[randomSentenceSelector].WordNumber;
+    let randomWord1 = Math.floor(Math.random() * (maxWord1 - minWord + 1)) + minWord;
+    //Wort 2
+    let maxWord2 = sentences[randomSentenceSelector].WordNumber;
+    let randomWord2 = Math.floor(Math.random() * (maxWord2 - minWord + 1)) + minWord;
+    //Variable mit Zufallszahl + passenden Text sodass der Speicherort im Objekt des jeweiligen Satzes gefunden wird
+    let textWord1 = "word" + randomWord1;
+    let textWord2 = "word" + randomWord2;
+    //Erstellung der spanischen Wörter
+    //Wort 1
+    let word1 = document.createElement("div");
+    word1.setAttribute("class", "word1");
+    word1.innerHTML = sentences[randomSentenceSelector].textWord1;
+    document.getElementById("content").appendChild(translationGerman);
+    console.log(textWord1);
+    //Wort 2
+    let word2 = document.createElement("div");
+    word2.setAttribute("class", "word1");
+    word2.innerHTML = sentences[randomSentenceSelector].textWord2;
+    document.getElementById("content").appendChild(translationGerman);
+    console.log(textWord2);
 }
 //# sourceMappingURL=script.js.map
